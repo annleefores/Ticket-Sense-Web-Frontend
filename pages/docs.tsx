3a3734fb7f46454a39b1cb162053b433f9b53d56
Annlee Fores <annleefores@gmail.com>
2022-09-17 12:28:01 +0530

    feat: docs update

--- pages/docs.tsx
+++ pages/docs.tsx
@@ -8,45 +8,69 @@ const Docs: NextPage = () => {
   return (
     <div>
       <Navbar />
-      <div className="flex flex-col  justify-center p-6 tracking-normal leading-relaxed  md:leading-loose mx-auto mt-[50px] md:max-w-[800px]">
+      <div className="flex flex-col  justify-center p-4 tracking-normal leading-relaxed  md:leading-loose mx-auto mt-[50px]  md:max-w-[800px]">
         <h1 className="text-3xl font-bold mb-4">Ticket Sense</h1>
 
-        <div className="mt-8">
-          <p className="text-lg font-semibold mb-4 ">
-            How to use Ticket Sense ?
-          </p>
-          <ul className="list-disc px-8">
-            <li>Login with Telegram</li>
-            <li>Proceed by entering your Telegram phone number.</li>
-            <li>
-              You will receive a message on your Telegram app; please approve
-              it.
-            </li>
-            <li>
-              Return to the website and accept the bot; you will be redirected
-              to the home page.
-            </li>
-            <li>Toggle the button to select the booking website.</li>
-            <li>
-              Enter the title of the film and select from the dropdown menu.
-            </li>
-            <li>
-              Enter the theater&apos;s address and select from the dropdown
-              menu.
-            </li>
-            <li>
-              Enter the name of the theatre and select from the dropdown menu.
-            </li>
-            <li>Choose a booking date</li>
-            <li>Click Enable Ticket Sense</li>
-          </ul>
-          <p className="mt-4 ">
-            That&rsquo;s it, you should see the movie poster appear below in the
-            added notifications section, make sure that its the same movie you
-            are looking for. When our system finds ticket going on sale we will
-            notify you through our telegram bot.
-          </p>
-        </div>
+        <p className="text-lg font-bold mt-6">How to use Ticket Sense ?</p>
+        <ul>
+          <li>
+            <p className="font-semibold mt-6">Login with Telegram</p>
+            <ul className="list-disc px-4 mt-1">
+              <li>
+                Select country and enter you telegram account phone number.
+              </li>
+              <li>Click next.</li>
+              <li>
+                You will get a message on telegram from Telegram Service
+                Notification.
+              </li>
+              <li>Read it and click confirm.</li>
+              <li>
+                Now go back to the web, if there&rsquo;s an accept page, accept
+                it after reading the conditions.
+              </li>
+            </ul>
+            <p className="mt-2">
+              Now you have successfully logged in with telegram.
+            </p>
+            <p>
+              To logout, go to Telegram Service Notification on you telegram app
+              and click terminate session.
+            </p>
+            <p>To fully cancel this service click on Disconnect.</p>
+          </li>
+          <li>
+            <p className="font-semibold mt-6">Using Ticket Sense</p>
+            <ul className="list-disc px-4 mt-1">
+              <li>Select the booking website.</li>
+              <li>
+                Enter the title of the film and select from the dropdown menu.
+              </li>
+              <li>
+                Enter the theater&apos;s location and select from the dropdown
+                menu.
+              </li>
+              <li>
+                Enter the name of the theatre and select from the dropdown menu.
+              </li>
+              <li>Choose a booking date</li>
+              <li>Click Enable Ticket Sense</li>
+            </ul>
+            <p className="mt-2">
+              That&rsquo;s it, you should see the movie poster appear below in
+              the added notifications section, make sure that its the same movie
+              you are looking for.
+            </p>
+            <p>
+              When our system finds tickets going on sale we will notify you
+              through our ticket sense telegram bot.
+            </p>
+            <p>
+              To delete the notification, simply tap on the poster and click the
+              delete option.
+            </p>
+          </li>
+        </ul>
       </div>
     </div>
   );
